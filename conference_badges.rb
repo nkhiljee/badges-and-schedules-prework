738d2f2ba99be5c439e1428a77cc398f417fe444
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 07:04:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,7 @@ end
 
 def batch_badge_creator(array)
   attendees = []
-  array.push { |attendees| print "Hello, my name is #{badge_maker(attendees)}."}
+  array.push { |attendees| 
+  print "Hello, my name is #{badge_maker(attendees)}."}
 end
 
